ee4a7e74eec7b365d7565d99b94581262f767b9a
Bethany Walker <reklaw.bethany@gmail.com>
2018-11-21 13:27:21 -0500

    Fixed using Firebase and using Firebase.Auth typos

--- Assets/AuthManager.cs
+++ Assets/AuthManager.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using FireBase;
-using FireBase.Auth;
+using Firebase;
+using Firebase.Auth;
 
 public class AuthManager : MonoBehaviour {
 
