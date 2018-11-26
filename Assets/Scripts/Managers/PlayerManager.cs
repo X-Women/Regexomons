1e9940969cbb1fbf75ce87278cc19ca609cd139d
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-11-26 18:37:54 -0500

    committing before pulling

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public static List<Player> playersList = new List<Player>();
+   
+    void Awake()
+    {
+        //clears list so not duplicating data
+        playersList.Clear();
+        DatabaseManager.sharedInstance.GetAllPlayers(data =>
+        {
+            //this has the players data
+            playersList = data;
+            Debug.Log("PlayerList: " + playersList[0].email);
+
+
+        });
+       // Router.Players().ChildAdded += AddedPlayer;
+
+
+    }
+    //void AddedPlayer(object sender, ChildChangedEventArgs args) {
+    //    if (args.Snapshot.Value == null) {
+    //        Debug.Log("There is no data for that node");
+    //    } else {
+    //        Debug.Log("A new player has been added");
+    //    }
+
+    //}
+}
+
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        //Debug.Log(TestQuest.CurrentUserId);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
