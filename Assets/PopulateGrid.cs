fe59bb5e7b2c181fa5b60fbfa21c5a1b099ee28f
Bethany Walker <reklaw.bethany@gmail.com>
2018-11-26 20:47:53 -0500

    Working on Regedex scroll view

--- Assets/PopulateGrid.cs
+++ Assets/PopulateGrid.cs
@@ -1,33 +1,57 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
 
 public class PopulateGrid : MonoBehaviour 
 {
-	public GameObject prefab; //this is our prefab object that will be exposed in the inspector
+	public GameObject prefabRegexomon; //this is our prefab object that will be exposed in the inspector
+    public GameObject scrollContainer;                                 //public GameObject scrollContainer;
 
-	public int numberToCreate; //number of objects to create.exposed in inspector
 
-	void Start () 
+    //public int numberToCreate = ManageUserId.CurrentUserId.Length; //number of objects to create.exposed in inspector
+
+    public List<PlayerRegexomon> regexomonList = new List<PlayerRegexomon>();
+
+    //void Populate()
+    void Awake()
 	{
-		Populate();
-	}
-	
+        regexomonList.Clear();
 
-	void Update () 
-	{
-		
-	}
+        var CurrentUserId = ManageUserId.CurrentUserId;
+        GameObject newObj; //Create Game object instance
+        newObj = (GameObject)Instantiate(prefabRegexomon, transform);
 
-	void Populate()
-	{
-		GameObject newObj; //Create Game object instance
+        Router.PlayerWithUID(CurrentUserId).Child("regexomon").GetValueAsync().ContinueWith(task =>
+        {
+            DataSnapshot regexomons = task.Result;
+            foreach (DataSnapshot regexomon in regexomons.Children)
+            {
+                newObj = (GameObject)Instantiate(prefabRegexomon, transform);
+                var regDictionary = (IDictionary<string, object>)regexomon.Value;
+                PlayerRegexomon newPlayerRegexomon = new PlayerRegexomon(regDictionary);
+                Debug.Log("DID I DO IT?" + newPlayerRegexomon.name);
+                regexomonList.Add(newPlayerRegexomon);
+            }
 
-		for(int i = 0; i < numberToCreate; i++)
-		{
-			newObj = (GameObject)Instantiate(prefab, transform); //Create new instances of our prefab until we've created as many as we specified
-			newObj.GetComponent<Image>().color = Random.ColorHSV(); // Randomize the color of our image
-		}
-	}
+        });
+        InitialiseUI();
+
+    }
+
+    void InitialiseUI() {
+        foreach (PlayerRegexomon regexomon in regexomonList) 
+        {
+            CreateRow(regexomon);
+        }
+    }
+
+    void CreateRow(PlayerRegexomon regexomon)
+    {
+        GameObject newReg = Instantiate(prefabRegexomon) as GameObject;
+        newReg.transform.SetParent(scrollContainer.transform, false);
+    }
+    
 }
--- Assets/Scripts/Managers/TestQuest.cs
+++ Assets/Scripts/Managers/TestQuest.cs
@@ -2,81 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
-using Firebase.Database;
 using Firebase.Auth;
+using System.Threading.Tasks;
 
 
-public class TestQuest : MonoBehaviour
-{
-    //Trying to get access to single user
+public class TestQuest : MonoBehaviour {
 
+    /*
+    Use QuestionManager.levelOneQuestions in you code to access the questions from the database
+    QuestionManager runs when login screen appears. It needs to run here because 
+    QuestionManager.levelOneQuestions needs to get it's data before use
+    */
 
-    //public static List<QuestionSet> levelOneQuestions = new List<QuestionSet>();
-
-
-    void Awake()
-    {
+    //this file is just for practice, and can be removed
+    void Awake () {
         var CurrentUserId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
         Debug.Log("CurrentUserId" + CurrentUserId);
-
-
-        Router.PlayerWithUID(CurrentUserId).GetValueAsync().ContinueWith(task =>
-        {
-            DataSnapshot user = task.Result;
-
-        //var playerDictionary = (IDictionary<string, object>)user.Value;
-        //Debug.Log("What is the playerDictionary : " + playerDictionary); //logs What is the playerDictionary : System.Collections.Generic.Dictionary`2[System.String,System.Object]
-        //Player newPlayer = new Player(playerDictionary);
-            //Player newPlayer = user.GetValue(Player);
-            //Debug.Log("What is the new Player : " + newPlayer); //logs nothing 
-        });
-
-        Router.PlayerWithUID(CurrentUserId).Child("regexomon").GetValueAsync().ContinueWith(task =>
-        {
-            DataSnapshot regexomons = task.Result;
-            foreach (DataSnapshot regexomon in regexomons.Children)
-            {
-                var regDictionary = (IDictionary<string, object>)regexomon.Value;
-                PlayerRegexomon newPlayerRegexomon = new PlayerRegexomon(regDictionary);
-                Debug.Log("DID I DO IT LALA?" + newPlayerRegexomon.name);
-            }
-
-        });
-
+        //Debug.Log("questionsCout: " + QuestionManager.levelOneQuestions.Count);
+        //Debug.Log("question 1: " + QuestionManager.levelOneQuestions[0].question);
+        //Debug.Log("answer 1: " + QuestionManager.levelOneQuestions[0].right);
+        //Debug.Log("wrong choice 1: " + QuestionManager.levelOneQuestions[0].wrong[0]);
+        //Debug.Log("wrong choice 2: " + QuestionManager.levelOneQuestions[0].wrong[1]);
+        //Debug.Log("wrong choice 3: " + QuestionManager.levelOneQuestions[0].wrong[2]); 
     }
-
 }
-
-
-
-
-
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using Firebase;
-//using Firebase.Auth;
-//using System.Threading.Tasks;
-
-
-//public class TestQuest : MonoBehaviour {
-
-//    /*
-//    Use QuestionManager.levelOneQuestions in you code to access the questions from the database
-//    QuestionManager runs when login screen appears. It needs to run here because 
-//    QuestionManager.levelOneQuestions needs to get it's data before use
-//    */
-
-//    //this file is just for practice, and can be removed
-//    void Awake () {
-//        var CurrentUserId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
-//        Debug.Log("CurrentUserId" + CurrentUserId);
-//        //Debug.Log("questionsCout: " + QuestionManager.levelOneQuestions.Count);
-//        //Debug.Log("question 1: " + QuestionManager.levelOneQuestions[0].question);
-//        //Debug.Log("answer 1: " + QuestionManager.levelOneQuestions[0].right);
-//        //Debug.Log("wrong choice 1: " + QuestionManager.levelOneQuestions[0].wrong[0]);
-//        //Debug.Log("wrong choice 2: " + QuestionManager.levelOneQuestions[0].wrong[1]);
-//        //Debug.Log("wrong choice 3: " + QuestionManager.levelOneQuestions[0].wrong[2]); 
-//    }
-//}
