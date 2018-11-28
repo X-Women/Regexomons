4463cc8b49f988054074c7b812ad4de99d6b5e16
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-11-28 15:49:23 -0500

    fixed and updated build settings, and added welcome scene

--- Assets/UserRegManager.cs
+++ Assets/UserRegManager.cs
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Firebase;
-using Firebase.Database;
-using Firebase.Auth;
-
-public class UserRegManager : MonoBehaviour {
-    public static List<PlayerRegexomon> regexomonList = new List<PlayerRegexomon>();
-    // Use this for initialization
-    void Awake()
-    {
-        regexomonList.Clear();
-
-        var CurrentUserId = UserIdManager.CurrentUserId;
-
-        Router.PlayerWithUID(CurrentUserId).Child("regexomon").GetValueAsync().ContinueWith(task =>
-        {
-            DataSnapshot regexomons = task.Result;
-            foreach (DataSnapshot regexomon in regexomons.Children)
-            {
-                var regDictionary = (IDictionary<string, object>)regexomon.Value;
-                PlayerRegexomon newPlayerRegexomon = new PlayerRegexomon(regDictionary);
-                regexomonList.Add(newPlayerRegexomon);
-            }
-
-        });
-
-    }
-}
-=======
 
 using System;
 using System.Collections;
@@ -78,4 +46,3 @@ public class UserRegManager : MonoBehaviour {
         });
     }
 }
->>>>>>> 947007366e5c3442235422fa9c1855a96d2e344d
